834d8281a05cd85a53f1f8307bd99aba152ddd1d
aki senpai | mommy gf <endercreeper4ut@gmail.com>
2017-11-26 17:20:23 -0500

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace SteakPE
+namespace SteakPE;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\player\PlayerJoinEvent;
@@ -11,35 +11,18 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
 class Main extends PluginBase implements Listener{
-public function onLoad ()
+public function onLoad()
 {
-	$this->getLogger () ->info ("SteakPE Loading") ;
+	$this->getLogger()->info ("SteakPE Loading") ;
 }
-public function onEnable ()
+public function onEnable()
 {
-    $this->getServer ()  ->getPluginManager () ->registerEvents ($this,$this);
-	$this->getLogger () ->info ("SteakPE Enabled") ;	
+    $this->getServer()->getPluginManager () ->registerEvents ($this,$this);
+	$this->getLogger()->info ("SteakPE Enabled") ;	
 }
-public function onDisable ()
+public function onDisable()
 {
-	$this->getLogger () ->info ("SteakPE Disabled") ;
+	$this->getLogger()->info ("SteakPE Disabled") ;
 }
-public function onCommand(CommandSender $sender,Command $cmd,$label,array $args) {
-	if($cmd->getName () == "steak") {
-		$sender->sendMessage("Processing Steak!");
-		if($cmd->getName() == "steak"){
-     $sender->getInventory()->addItem(Item::get(364,0,2));
-	 $sender->sendMessage(TextFormat::BOLD."You have just recieved 64 steak!");
-     if(!$sender instanceof Player){
-          $sender->sendMessage("This Command Only Works for players! Please perform this command IN GAME!");
-     }else{
-	  if(!isset($args[0]) or (is_int ($args[0]) and $args[0] > 0)) {
-                $args[0] = 64;
-		}
-          $sender->getInventory()->addItem(Item::get(364,0,4));
-          $sender->sendMessage("You have just recieved". count($args[0]) . " steak!");
-		  }
-		  return true}
-}
-}
+	
 }
