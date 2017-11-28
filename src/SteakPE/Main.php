3738163ffe64a5af150e404626cf3b7c7eff5ddd
aki senpai | mommy gf <endercreeper4ut@gmail.com>
2017-11-28 10:36:44 -0500

    Update and rename src/Main.php to src/SteakPE/Main.php

--- src/SteakPE/Main.php
+++ src/SteakPE/Main.php
@@ -8,17 +8,20 @@ use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\command\Command;
+use pocketmine\event\Listener;
 use pocketmine\command\CommandSender;
+use SteakPE\cmd\SteakCommand;
 
 class Main extends PluginBase implements Listener{
 public function onLoad()
 {
-	$this->getLogger()->info ("SteakPE Loading") ;
+	$this->getLogger()->info("SteakPE Loading");
 }
 public function onEnable()
 {
-    $this->getServer()->getPluginManager () ->registerEvents ($this,$this);
-	$this->getLogger()->info ("SteakPE Enabled") ;	
+        $this->getServer()->getPluginManager () ->registerEvents ($this,$this);
+	$this->getLogger()->info("SteakPE Enabled");
+	$this->getServer()->getCommandMap()->register("steak", new SteakCommand($this));
 }
 public function onDisable()
 {
